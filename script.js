b08dadf0c4511f7765fb0c39f3e66c1c313701b7
AnsonAMS <anson_tohata@hotmail.com>
2020-10-22 14:01:58 +0200

    Changing script

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//  const toggle = document.getElementById('toggle');
-//  const close = document.getElementById('close');
-//  const open = document.getElementById('open');
-//  const modal = document.getElementById('modal');
-
-//  // Toggle nav
-//  toggle.addEventListener('click', () => 
-//     document.body.classList('show-nav')
-// );
-
-// // Show modal
-// open.addEventListener('click', () => modal.classList.add('show-modal'));
-
-// // Hide modal
-// closew.addEventListener('click', () => modal.classList.remove('show-modal'));
-
-// // Hide modal on outside click 
-// window.addEventListener('click', e =>
-//     e.target == modal ? modal.classList.remove('show-modal') : false 
-// );
-
 const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
